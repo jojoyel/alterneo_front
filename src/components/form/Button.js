fb0e080c8012d171a6f35c9ed7c61df4e0dbc993
Jojoyel <jolalle974@gmail.com>
2022-02-12 11:29:50 +0100

    Now supporting material icons in buttons

--- src/components/form/Button.js
+++ src/components/form/Button.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './../../styles/_button.scss'
 
 const Button = (props) => {
-	if (props.icon !== null) {
+	if (props.icon !== undefined) {
 		return (
-			<button className={props.style === undefined ? '' : props.style} onClick={props.callback}>
-				<span className="material-icons">
-					{props.icon}
+			<button className={props.style === undefined ? '' : props.style + " iconed"} onClick={props.callback}>
+				<span className="button-content">
+					<span className="material-icons">
+						{props.icon}
+					</span>
+					{props.text}
 				</span>
-				{props.text}
 			</button>
 		);
 	} else {
