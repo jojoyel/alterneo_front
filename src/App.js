cbaacd4ce3ac510890e495b7283ae45d181644c4
Jojoyel <jolalle974@gmail.com>
2022-02-13 10:12:01 +0100

    Modified JobOfferSearchCard.js, added icons for priority & limit, changed default a in style.scss

--- src/App.js
+++ src/App.js
@@ -5,26 +5,30 @@ import JobOfferSearchCard from "./components/JobOfferSearchCard";
 const test = {
 	"company": {
 		"company_name": "Nextfor",
+		"company_id": 12334,
 		"avatar_url": "https://previews.123rf.com/images/sabelskaya/sabelskaya1603/sabelskaya160300156/54119905-carr%C3%A9-neon-lumi%C3%A8re-bleue-au-n%C3%A9on-.jpg"
 	},
 	"offer_name": "Développeur Android H/F",
 	"contract_type": "CDD",
 	"contract_duration": "1 mois",
-	"requirements": "Android natif, bac +5"
+	"requirements": "Android natif, bac +5",
+	"emergency": 1,
+	"limited": 20
 }
 const App = () => {
 	return (
 		<div>
-			<Button style="test" text="Normal">
+			<Button style="" text="Normal">
 			</Button>
-			<Button style="outlined test" text="outlined">
+			<Button style="outlined" text="outlined">
 			</Button>
-			<Button style="rounded test" text="rounded">
+			<Button style="rounded" text="rounded">
 			</Button>
-			<Button style="rounded test outlined" text="rounded outlined">
+			<Button style="rounded outlined" text="rounded outlined">
 			</Button>
 			<JobOfferSearchCard company={test.company} offer_name={test.offer_name} contract_type={test.contract_type}
-			                    contract_duration={test.contract_duration} requirements={test.requirements}/>
+			                    contract_duration={test.contract_duration} requirements={test.requirements}
+			                    emergency={test.emergency} limited={test.limited}/>
 		</div>
 	);
 };
--- src/components/JobOfferSearchCard.js
+++ src/components/JobOfferSearchCard.js
@@ -2,32 +2,44 @@ import React from 'react';
 import Button from "./form/Button";
 import '../../src/styles/_card.scss';
 
-const test = {
-	"company": {
-		"company_name": "Nextfor",
-		"avatar_url": "https://lh3.googleusercontent.com/ogw/ADea4I7ATXeycU9QtgOPRkGR4xdtEOXzs91p1H0fAoHVHQ=s64-c-mo"
-	},
-	"offer_name": "Développeur Android H/F",
-	"contract_type": "CDD",
-	"contract_duration": "1 mois",
-	"requirements": "Android natif, bac +5"
-}
-
 const JobOfferSearchCard = (props) => {
-	return (
-		<div className="card test">
-			<img src={props.company.avatar_url} alt={"Image de l'entreprise " + props.company.company_name}/>
-			<div>
-				<div>
-					<p className="offer-name">{props.offer_name}</p>
-					<p className="">{props.contract_type} / {props.company.company_name}</p>
-					<p>{props.contract_duration}</p>
-					<p>{props.requirements}</p>
+	if (props.emergency !== undefined || props.limited !== undefined) {
+		return (
+			<div className="card">
+				<img className="company-logo" src={props.company.avatar_url}
+				     alt={"Image de l'entreprise " + props.company.company_name}/>
+				<div className="centered-part">
+					<div>
+						<p className="offer-name">{props.offer_name}</p>
+						<p className="">{props.contract_type} / <a href="#">{props.company.company_name}</a></p>
+						<p>{props.contract_duration}</p>
+						<p>{props.requirements}</p>
+					</div>
+					<Button text="Plus de détails" style="rounded" icon="chevron_right"/>
 				</div>
-				<Button text="Plus de détails" style="rounded" icon="chevron_right"/>
+				<div className="right-part">
+					<span className="material-icons">priority_high</span>
+					<span className="material-icons">groups</span>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div className="card">
+				<img className="company-logo" src={props.company.avatar_url}
+				     alt={"Image de l'entreprise " + props.company.company_name}/>
+				<div className="centered-part">
+					<div>
+						<p className="offer-name">{props.offer_name}</p>
+						<p className="">{props.contract_type} / <a href="#">{props.company.company_name}</a></p>
+						<p>{props.contract_duration}</p>
+						<p>{props.requirements}</p>
+					</div>
+					<Button text="Plus de détails" style="rounded" icon="chevron_right"/>
+				</div>
+			</div>
+		);
+	}
 };
 
 export default JobOfferSearchCard;
